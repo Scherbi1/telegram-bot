2b098c4c82f786c9f529c6ec94db93d5d280af50
Scherbi <shherbakov99ilya@mail.ru>
2023-01-05 20:25:36 +0300

    Вроде как сделал, но  почему то ошибка, я не знаю почему

--- src/main/resources/liquibase/scripts/Notification_Task.sql
+++ src/main/resources/liquibase/scripts/Notification_Task.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset scherbakov:1
+--changeset MyUsers:1
 CREATE TABLE notification_task(
                                   id SERIAL NOT NULL ,
                                   chatId bigint NOT NULL ,
